37468b584bd8bef8082b54777a34a8ae4cedc659
Hugh Halford-Thompson <hughht5@gmail.com>
2011-04-02 18:14:44 +0100

    first

---
+++ friends.php
@@ -0,0 +1,13 @@
+<html>
+
+    <div id="fb-root"></div>
+    <script src="http://connect.facebook.net/en_US/all.js"></script>
+    <script>
+        FB.init({
+            appId  : '215359871811504',
+            status : true, // check login status
+            cookie : true, // enable cookies to allow the server to access the session
+            xfbml  : true  // parse XFBML
+        });
+    </script>
+</html>
