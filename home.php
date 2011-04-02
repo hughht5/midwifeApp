6938d73ef15457891415236a113d12a485c414e6
Hugh Halford-Thompson <hughht5@gmail.com>
2011-04-02 13:55:54 +0100

    first

--- home.php
+++ home.php
@@ -2,9 +2,17 @@
 
 include("fbmain.php");
 
+
+
+$friends = $facebook->api('/me/friends');
+//print_r($friends);
+
+for ($i = 0; $i <= count($friends[data]) - 1; $i++) {
+    echo $friends[data][$i][gender];
+}
+
 $aboutTest = $facebook->api('/61411159');
 print_r($aboutTest);
 
-$friends = $facebook->api('/me/friends');
-print_r($friends);
+//for each friend count if male or female or unknown
 ?>
