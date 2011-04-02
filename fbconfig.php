fc35cb4db656800f48b171c6d78646df1b2a060e
Hugh Halford-Thompson <hughht5@gmail.com>
2011-04-02 12:15:24 +0100

    first

---
+++ fbconfig.php
@@ -0,0 +1,7 @@
+<?php
+
+$app_id = "169864479731045";
+$api = "86b4a970ccc189970a8d9d5dd715d5de";
+$secret = "90579676045a71402d9d5fc8497cf091";
+$canvas_page = "http://apps.facebook.com/placeyourselves/addNew.php";
+?>
