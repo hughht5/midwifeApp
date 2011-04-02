8bab2d9ea11c0ca71bb73f207afe58290f448750
unknown <Callum@.(none)>
2011-04-02 12:45:45 +0100

    =hugh is an idiot

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-	include("facebook/fbmain.php");
-	include("facebook/fbconfig.php");
-	$auth_url = "http://www.facebook.com/dialog/oauth?client_id="
-                . $app_id . "&redirect_uri=" . urlencode($canvas_page) . "&scope=friends_location,user_location,publish_stream";
-    $signed_request = $_REQUEST["signed_request"];
-    list($encoded_sig, $payload) = explode('.', $signed_request, 2);
-    $data = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);       
-    if (empty($data["user_id"])) {
-        echo("<script> top.location.href='" . $auth_url . "'</script>");
-    } else {
-        echo "<meta http-equiv=\"REFRESH\" content=\"0;url=$canvas_page\">";
-    }
+	include("fbmain.php");
+	include("fbconfig.php");
+	$auth_url = "http://www.facebook.com/dialog/oauth?client_id=" . $app_id . "&redirect_uri=" . urlencode($canvas_page) . "&scope=friends_location,user_location,publish_stream";
+	$signed_request = $_REQUEST["signed_request"];
+	list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+	$data = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);       
+	if(empty($data["user_id"])) {
+		echo("<script> top.location.href='" . $auth_url . "'</script>");
+	} else {
+		echo "<meta http-equiv=\"REFRESH\" content=\"0;url=$canvas_page\">";
+	}
+	
+	
+	
