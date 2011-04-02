476a6e581e292d198bb435fa3c153cfd4daa4a46
Hugh Halford-Thompson <hughht5@gmail.com>
2011-04-02 13:49:06 +0100

    first

--- home.php
+++ home.php
@@ -2,8 +2,8 @@
 
 include("fbmain.php");
 
-$aboutTest = $facebook->api('/61411159/about');
-print_r($aboutTest);
+//$aboutTest = $facebook->api('/61411159/about');
+//print_r($aboutTest);
 
 $friends = $facebook->api('/me/friends');
 print_r($friends);
