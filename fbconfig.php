b33432bbd3efc1ecff66f46e5cbe03a867e259da
Hugh Halford-Thompson <hughht5@gmail.com>
2011-04-02 12:41:31 +0100

    first

--- fbconfig.php
+++ fbconfig.php
@@ -3,5 +3,5 @@
 $app_id = "215359871811504";
 $api = "fe16e8696c59ccfad53c70a51df4a079";
 $secret = "aee13ae43f42d5e24c23ed93c6d8d271";
-$canvas_page = "http://apps.facebook.com/midwife/";
+$canvas_page = "http://apps.facebook.com/midwife/home.php";
 ?>
