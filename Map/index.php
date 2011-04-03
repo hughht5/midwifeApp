0bb37ca39aba80b9d727ac7d50a2b023998c9684
unknown <Callum@.(none)>
2011-04-03 12:54:00 +0100

    =map javascript

--- Map/index.php
+++ Map/index.php
@@ -74,7 +74,7 @@
   <body>
     <script>
 		if(window.location.hash.length == 0) {
-			top.location.href="https://www.facebook.com/dialog/oauth?client_id=215359871811504&redirect_uri=http://apps.facebook.com/midwife&response_type=token";
+			top.location.href="https://www.facebook.com/dialog/oauth?client_id=215359871811504&redirect_uri=http://apps.facebook.com/midwife/&response_type=token";
 		} else {
 			alert("WOOP WOOP");
 		}
