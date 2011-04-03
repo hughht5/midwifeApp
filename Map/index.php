1f9a319d0c62f021cc4876ebbbc90cf0697c697a
unknown <Callum@.(none)>
2011-04-03 19:19:03 +0100

    =rename

--- Map/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-  </head>
-  <body>  
-    <script>
-		top.location.href="https://www.facebook.com/dialog/oauth?client_id=215359871811504&redirect_uri=http://apps.facebook.com/stateoftheworld/map.php&response_type=token";
-	</script>
-  </body>
-</html>
