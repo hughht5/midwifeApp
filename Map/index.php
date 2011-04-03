3aa0e718df72f6848a3206310f5c28fdbe2e2352
unknown <Callum@.(none)>
2011-04-03 13:35:15 +0100

    =map javascript

--- Map/index.php
+++ Map/index.php
@@ -2,6 +2,7 @@
   <head>
   </head>
   <body>
+  
     <script>
 		top.location.href="https://www.facebook.com/dialog/oauth?client_id=215359871811504&redirect_uri=http://apps.facebook.com/midwife/map.php&response_type=token&type=web_server";
 	</script>
