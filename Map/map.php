24a71389f97cc8a75c32fa65aefb00a5de1bb3d0
unknown <Callum@.(none)>
2011-04-03 11:32:31 +0100

    =map javascript

--- Map/map.php
+++ Map/map.php
@@ -26,22 +26,36 @@
 				visualization.draw(data, options);
 				//click event
 				google.visualization.events.addListener(visualization, 'regionClick', function(region) {
+					//change zoomed region to region selected
 					options['region'] = region.region;
-					//TODO display loading graphic
-					visualization.draw(data, options);
+					//draw to a second buffer and flip when drawing is done
+					visualization2 = new google.visualization.GeoMap(document.getElementById('visualization2'));					
+					google.visualization.events.addListener(visualization2,'drawingDone', function(){
+						setTimeout(function(){
+							$("#visualization").css('z-index', '0');
+							$("#visualization2").css('z-index', '1');
+						}, 2000);						
+					});
+					visualization2.draw(data, options);					
 					//TODO get amount of friends
-					var friends = 100;
+					var friends = 100;					
 					//TODO find out boundaries in which to draw					
+					
 					//TODO place stick men inside
-					$("#animation").html('animation');
+					//$("#animation").html('animation');
+					
 					//TODO fade random ones away based on friend count and mortality rate
+					
 					//TODO bring up dialog for click through/ i agree .etc
+					//$("#doyouagree").html("showdialog");
 				});			
-			}			
+			}
     </script>
   </head>
   <body>
-    <div id="animation" style="position:absolute;z-index:1">lol</div>
-	<div id="visualization" style="z-index:0"></div>
+	<div id="doyouagree" style="position:absolute;z-index:3"></div>
+    <div id="animation" style="position:absolute;z-index:2"></div>
+	<div id="visualization" style="position:absolute;z-index:1"></div>
+	<div id="visualization2" style="position:absolute;z-index:0"></div>	
   </body>
 </html>
